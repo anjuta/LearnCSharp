be6d9706f803ecc0e342e4fafb3c57c01d94ab9d
anjuta <anjuta9526@gmail.com>
2021-01-04 13:35:21 +0200

    MethodLearning

---
+++ MethodPrintLine/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MethodPrintLine
+{
+    //Write the method that prints a string to the screen
+    //Symbols that make up the string and their number are entered by the user
+    class Program
+    {
+        static void PrintLine(string symbol, uint symbolsCount)
+        {
+            for (int i = 0; i < symbolsCount; i++)
+            {
+                Console.Write(symbol); 
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the symbol");
+            string symbol = Console.ReadLine();
+
+            Console.WriteLine("\nEnter the the number of symbols");
+            uint symbolsCount = uint.Parse(Console.ReadLine());
+
+            PrintLine(symbol, symbolsCount);
+        }
+    }
+}
