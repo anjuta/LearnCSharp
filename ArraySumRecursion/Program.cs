fec6a0dad003afe01764a3f83b84fce4dae85a38
anjuta <anjuta9526@gmail.com>
2021-01-06 14:32:01 +0200

    Recursion

---
+++ ArraySumRecursion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ArraySumRecursion
+{
+    // Find the sum of the elemnts of an array using recursion
+    class Program
+    {
+        static int Sum(int[] myArray, int i = 0)
+        {
+            if (i >= myArray.Length)
+                return 0;
+            int result = Sum(myArray, i + 1);
+            return myArray[i] + result;
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = {1, 4, 6};
+            int result = Sum(myArray);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ SumOfDigitsOfNumRecursion/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SumOfDigitsOfNumRecursion
+{
+    // Find the sum of the digits of the number using recursion
+    class Program
+    {
+        /*
+        static int Foo(int value)
+        {
+            int result = 0;
+            if (value < 10)
+                return value;
+            
+            int digit = value % 10;
+            int nextValue = value / 10;
+            return 10 + Foo(nextValue);
+        }
+        */
+
+        static int Bar (int value)
+        {
+            if (value < 10)
+                return value;
+
+            return value % 10 + Bar(value / 10);
+        }
+        static void Main(string[] args)
+        {
+            int myValue = 561;
+            int result = Bar(myValue);
+            Console.WriteLine(result);
+        }
+    }
+}
